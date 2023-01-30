a63c18017b2dd5b3fea27f12fa34e4e31f0d9f9f
LucasCruz <lca.snf21@uea.edu.br>
2023-01-30 14:46:16 -0400

    add 'cors' to backend

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from urls import urls
-from database.db import create_db, db_connect
+from database.db import db_connect
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = b'kdakoaskoa_2821'
-
-
 db_connect()
 urls(app)
 
