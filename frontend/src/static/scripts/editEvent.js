2423c8abb87961934b64ee0eb144d3f9afff2aa7
LucasCruz <lca.snf21@uea.edu.br>
2023-01-31 08:26:10 -0400

    request edit event on js

---
+++ frontend/src/static/scripts/editEvent.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form-edit-event')
+const id = document.getElementById('event-id').textContent
+
+
+const editEvent = async (data) => {
+    const endpoint = 'edit_event/' 
+    await fetch('http://localhost:8000/'+endpoint+id, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        mode:"cors",
+        headers:{
+            "Content-type": "application/json",
+        }, 
+     })
+}
+
+
+form.addEventListener('submit', async (event)=>{
+    event.preventDefault();
+    
+
+    const title = form['title'].value
+    const description = form['description'].value
+    const date = form['date'].value
+    const hour = form['hour'].value
+    const user_id = form['user_id'].value
+    
+
+    const data = {
+        "title": title,
+        "description":description, 
+        "date": date, 
+        "hour": hour, 
+        "user_id": parseInt(user_id)
+    }
+
+    
+    editEvent(data).then(window.history.back())
+
+    
+})
