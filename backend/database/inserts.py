7a715d9c950e446cbc1ac1b5cc0cf7bfbb73d138
LucasCruz <lca.snf21@uea.edu.br>
2023-01-31 10:05:04 -0400

    delete functions on 'database'

--- backend/database/inserts.py
+++
@@ -1,6 +0,0 @@
-def insert_user(cursor, user_name, user_email, user_password):
-     cursor.execute("insert into user (name, email, password, status) values (?,?,?,?)", [user_name, user_email, user_password, 0])
-
-
-def insert_event(cursor, event_title, event_description, event_date, user_id):
-     cursor.execute("INSERT INTO event (title, description, date, user) VALUES (?,?,?,?)", [event_title, event_description, event_date, user_id])
