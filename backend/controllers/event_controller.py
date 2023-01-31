69b595919a9b52f7007782b3e6840c942762a0ba
LucasCruz <lca.snf21@uea.edu.br>
2023-01-31 08:28:43 -0400

    refactor event controller for single responsability

--- backend/controllers/event_controller.py
+++ backend/controllers/event_controller.py
@@ -3,85 +3,39 @@ from database.querys import *
 from database.inserts import *
 from database.deletes import *
 from database.updates import *
-from database import db  
-from datetime import datetime
+from database.db import db_connect  
+from usecases.event.add_event_usecase import *
+from usecases.event.get_one_event_usecase import *
+from usecases.event.get_all_event_user_usecase import *
+from usecases.event.edit_event_usecase import *
+from usecases.event.del_event_usecase import *
+
+
+
+
 
 def add_event():
-        body = dict(request.get_json(force=True))
-        event_title = body.get("title")
-        event_description = body.get("description")
-        event_date = body.get("date")
-        event_hour = body.get("hour")
-        user_id = body.get("user_id")
-
-        if not event_title:
-            event_title = "Meu evento"
-
-        
-        date_string = event_date+" "+event_hour
-        
-        event_date = datetime.strptime(date_string, '%Y-%m-%d %H:%M').strftime('%d/%m/%y %H:%M')
-
-
-        if(len(event_title) != 0 and len(event_date) != 0):
-            _db = db.db_connect()
-            insert_event(_db["cursor"], event_title, event_description, event_date, user_id)
-            _db["connection"].commit()
-            _db["connection"].close()
-            return jsonify({'message': 'event created'}), 201
-        else:
-            return jsonify({}), 400
+        data = dict(request.get_json(force=True))
+        response = add_event_usecase(data)
+        return response
 
 
 def get_event(id):
-        _db = db.db_connect()
-        event = get_one_event(_db["cursor"], id)
-        _db["connection"].close()
-        if "id" in event:
-            return jsonify(event)
-        else:
-            return jsonify({}), 200
+    response = get_one_event_usecase(id)
+    return response
+        
 
 def list_events(id):
-        print(id)
-        print("FEZ O GET")
-        _db = db.db_connect()
-        events = get_events(_db["cursor"], id)
-        _db["connection"].close()
-
-        print(events)
-        if(events):
-            return jsonify(events), 200
-        else:
-            return jsonify({}), 200
+        response = get_all_event_user_usecase(id)
+        return response
 
 def edit_event(id):
-    if request.method == "PUT":
-        body = dict(request.get_json(force=True))
-        event_title = body.get("title")
-        event_description = body.get("description")
-        event_date = body.get("date")
-        try:
-            _db = db.db_connect()
-            user = update_event(_db["cursor"], id, event_title, event_description, event_date)
-            _db["connection"].commit()
-            _db["connection"].close()
-            return jsonify({}), 200
-        except Exception:
-            return jsonify({}), 400
-    else:
-        return jsonify({}), 400
+    data = dict(request.get_json(force=True))
+    response = edit_event_usecase(id, data)
+    return response
+
 
 def del_event(id):
-        if request.method == "DELETE":      
-            try:
-                _db = db.db_connect()
-                event = get_one_event(_db["cursor"], id)
-                delete_event(_db["cursor"], id)
-                _db["connection"].commit()
-                _db["connection"].close()
-                return jsonify({}), 202
-            except Exception:
-                _db["connection"].close()
-                return jsonify({}), 404
-        return jsonify({}), 404
+        response = del_event_usecase(id)
+        return response    
+            
