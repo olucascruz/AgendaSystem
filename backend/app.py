154233986c1088946444b2949b2cdb00ea0526c9
LucasCruz <lca.snf21@uea.edu.br>
2023-02-16 14:32:36 -0400

    perf: update

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
 from urls import urls
-from database.db import db_connect
+from database.db import init_app
 
-app = Flask(__name__)
-CORS(app)
-app.secret_key = b'kdakoaskoa_2821'
-db_connect()
-urls(app)
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    app.secret_key = b'kdakoaskoa_2821'
+    init_app(app) 
+    return app
 
+app = create_app()
+with app.app_context():
+    urls(app)
 
 
 
--- backend/database/db.py
+++ backend/database/db.py
@@ -2,36 +2,42 @@ import sqlite3
 import pathlib
 import urllib.parse
 from .create_db import create_db
+from flask import g
 
-def db_connect():
-    PATH_DB = "agenda_db.db"
 
+def get_db():
+    PATH_DB =  'agenda_db.db'
 
     def _path_to_uri(path):
         path = pathlib.Path(path)
         if path.is_absolute():
             return path.as_uri()
         return 'file:' + urllib.parse.quote(path.as_posix(), safe=':/')
+
+    
     try:
-        print(_path_to_uri(PATH_DB))
-        check_db_exist = sqlite3.connect(_path_to_uri(PATH_DB)+"?mode=rw", uri=True)
-        print("db connected")
+        check_db_exist = sqlite3.connect(_path_to_uri(PATH_DB)+'?mode=rw', uri=True)
+        print('db connected')
         check_db_exist.close()
     except sqlite3.OperationalError:
         create_db()
 
+    if 'connection' not in g:
+        print("connected")
+        g.connection = sqlite3.connect(PATH_DB, check_same_thread=False,
+        detect_types=sqlite3.PARSE_DECLTYPES)
+        g.connection.row_factory = sqlite3.Row
 
-    connection = sqlite3.connect(PATH_DB)
-    cursor = connection.cursor()
-
-
-    DB = {
-        "connection": connection,
-        "cursor":cursor,
-    }
-
-    return DB
+    return g.connection
 
 
+def close_db(e=None):
+    connection = g.pop('connection', None)
+
+    if connection is not None:
+        connection.close()
 
 
+def init_app(app):
+    get_db()
+    app.teardown_appcontext(close_db)
